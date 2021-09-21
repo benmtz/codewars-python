6ebf20e70e1fc3553763a9faef458f199a6a3607
Ben <ben.mertz.dev@gmail.com>
2021-09-21 07:30:39 +0200

    docs: add greed solution

--- katas/5-greed-is-good.py
+++ katas/5-greed-is-good.py
@@ -39,6 +39,15 @@ def extract_score(counts, dice_value, dice_count, value):
         result += value
     return result
 
+# Best one from solutions --> there is a count method
+# def score(dice):
+#     return dice.count(1)//3 * 1000 + dice.count(1)%3 * 100 \
+#            + dice.count(2)//3 * 200 \
+#            + dice.count(3)//3 * 300 \
+#            + dice.count(4)//3 * 400 \
+#            + dice.count(5)//3 * 500 + dice.count(5)%3 * 50 \
+#            + dice.count(6)//3 * 600 \
+
 
 def score(dice):
     result = 0
